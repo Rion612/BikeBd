0e4f6557cb8fbffbd02cdde4b7a9e61b0a18d6ef
Md. Nazmul Hasan <mdnazmul612@gmail.com>
2021-05-23 12:38:11 +0600

    commit

--- admin/src/Container/Showroom/Showroom.js
+++ admin/src/Container/Showroom/Showroom.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import './style.css'
 
 function Showroom() {
     return (
-        <div>
+        <div className="showroom">
             <div className="title"><h3>Bike Showrooms</h3></div>           
         </div>
     )
