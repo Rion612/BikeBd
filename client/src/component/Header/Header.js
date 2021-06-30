70067e3a0b9f53c4473d0c9f557151a74e31fe78
Md. Nazmul Hasan <mdnazmul612@gmail.com>
2021-06-30 11:23:29 +0600

    c

--- client/src/component/Header/Header.js
+++ client/src/component/Header/Header.js
@@ -14,7 +14,7 @@ const Header = () => {
                 
                 <Navbar style={{ backgroundColor: '#060b26' }} variant="dark">
                 <button className="mobile-menu-icon" onClick={showBar}>{ isMobile ? <i className="fa fa-times"></i> : <i className="fa fa-bars"></i>}</button>
-                    <Navbar.Brand href="#home" className="mr-auto" style={{ fontSize: '30px' }}>Bongo Bike</Navbar.Brand>
+                    <Navbar.Brand href="#home" className="mr-auto" style={{ fontSize: '30px' }}>BikePedia</Navbar.Brand>
                     <Form inline>
                         <FormControl type="text" placeholder="Search" className="mr-sm-2" />
                         <Button variant="outline-info">Search</Button>
