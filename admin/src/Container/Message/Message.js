d48a0f2f7a064c9839ab292042b63f67afcbb9e0
Md. Nazmul hasan <mdnazmul.hasan@bjitgroup.com>
2021-11-25 15:48:08 +0600

    commit

--- admin/src/Container/Message/Message.js
+++ admin/src/Container/Message/Message.js
@@ -1,17 +1,22 @@
 import React, { useEffect, useState } from 'react'
 import { Table } from 'react-bootstrap';
 import axios from '../../helpers/axios';
-import { IoPersonCircleSharp } from 'react-icons/io5'
+import { IoPersonCircleSharp } from 'react-icons/io5';
+import {RiDeleteBin5Fill} from 'react-icons/ri'
 import './message.css';
 
 const Message = (props) => {
     const [messages, setMessages] = useState([]);
+    const [mailContent, setMailContent] = useState({});
     useEffect(async () => {
         const res = await axios.get('/get/all/messages');
         if (res.status == 200) {
             setMessages(res.data.data);
         }
     }, [])
+    const contentChange = (mail) =>{
+        setMailContent(mail);
+    }
     return (
         <div style={{ marginTop: '80px' }}>
             <div className="title"><h3>Inbox</h3></div>
@@ -25,7 +30,7 @@ const Message = (props) => {
                         {
                             messages.map((item, index) => {
                                 return (
-                                    <div className="singleMail" key={index}>
+                                    <div className={item.status ? 'singleReadMail': 'singleUnreadMail'} key={index} onClick={()=>contentChange(item)}>
                                         <div>
                                             <IoPersonCircleSharp
                                                 size={60}
@@ -34,9 +39,9 @@ const Message = (props) => {
                                         </div>
                                         <div>
                                             <div style={{ display: 'flex', padding: '10px' }}>
-                                                <div style={{width:'70%',wordWrap:'break-word'}}>{item?.email}</div>
-                                                <div style={{width: '30%' }}>
-                                                    <p style={{width:'100%',textAlign:'right'}}>24/11/2021</p>
+                                                <div style={{ width: '70%', wordWrap: 'break-word' }}>{item?.email}</div>
+                                                <div style={{ width: '30%' }}>
+                                                    <p style={{ width: '100%', textAlign: 'right' }}>24/11/2021</p>
                                                 </div>
                                             </div>
                                             <div style={{ paddingLeft: '10px' }}>
@@ -50,9 +55,26 @@ const Message = (props) => {
                         }
                     </div>
                     <div className="mailDetails">
-                        <div>
-                            <img src={process.env.PUBLIC_URL + '/mailbox.jpg'} height="100%" width="100%" />
-                        </div>
+                        {
+                            Object.keys(mailContent).length == 0 ?
+                                <div>
+                                    <img src={process.env.PUBLIC_URL + '/mailbox.jpg'} height="100%" width="100%" />
+                                </div> :
+                               <div>
+                                   <div style={{display:'flex', justifyContent:'space-between'}}>
+                                       <div>
+                                            <h3>{mailContent?.subject}</h3>
+                                       </div>
+                                       <div>
+                                            <RiDeleteBin5Fill size={40} color="red"/>
+                                       </div>
+                                   </div>
+                                   <div>
+                                       <p>From: {mailContent?.email}</p>
+                                   </div>
+                               </div>
+                        }
+
 
                     </div>
                 </div>
